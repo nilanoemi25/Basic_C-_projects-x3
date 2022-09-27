4ce521475622da86242924175dd3585dd3829e16
nila.noemi25 <nila.noemi25@gmail.com>
2022-09-27 15:15:13 +0100

    HW
    Hello World and challenege

---
+++ HelloWorld1/HelloWorld1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorld1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("I am using \" an escape \" character."); 
+            Console.ReadLine(); 
+        }
+    }
+}
---
+++ SampleApplication.cs
@@ -0,0 +1,9 @@
+using System; 
+
+class SampleAppliation 
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
