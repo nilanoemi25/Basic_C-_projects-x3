dd198f8dae91d076ba409d37365c6aff8adff593
nila.noemi25 <nila.noemi25@gmail.com>
2022-09-28 15:31:17 +0100

    Maths Operation

---
+++ MathOperationAssignment/MathOperationAssignment/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace MathOperationAssignment
+{
+    class Program
+    {
+        static void Main()
+        {
+            //
+            //Console.WriteLine("Choose a number."); 
+            //string input = Console.ReadLine();
+            //int num1 = Convert.ToInt32(input);
+            //int result = num1 * 50;
+            //Console.WriteLine(result);
+            //Console.ReadLine();
+
+            //Console.WriteLine("Choose a number.");
+            //string input1 = Console.ReadLine();
+            //int num2 = Convert.ToInt32(input1);
+            //int result1 = num2 +25;
+            //Console.WriteLine(result1);
+            //Console.ReadLine();
+
+            //Console.WriteLine("Choose a number.");
+            //string input2 = Console.ReadLine();
+            //int num3 = Convert.ToInt32(input2);
+            //double result2 = num3 / 12.5; 
+            //Console.WriteLine(result2);
+            //Console.ReadLine();
+
+            //Console.WriteLine("Choose a number.");
+            //string input3 = Console.ReadLine();
+            //int input4 = Convert.ToInt32(input3); 
+            //bool thisOr = input4 > 50; 
+            //Console.WriteLine(thisOr);
+            //Console.ReadLine();
+
+            Console.WriteLine("Choose a number:");
+            string number = Console.ReadLine();
+            int numb = Convert.ToInt32(number);
+            int result = numb % 7;
+            Console.WriteLine(result);
+            Console.ReadLine(); 
+
+        }
+    }
+}
