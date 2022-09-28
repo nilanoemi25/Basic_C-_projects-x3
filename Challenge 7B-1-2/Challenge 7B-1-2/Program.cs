7675a642b85648c21211177b288fb844c387610a
nila.noemi25 <nila.noemi25@gmail.com>
2022-09-28 14:34:01 +0100

    Challenge with Maths Operators

---
+++ Challenge 7B-1-2/Challenge 7B-1-2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Challenge_7B_1_2
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            //Challenge 7B 1
+            //int num = 1;
+            //int num2 = 3;
+            //int answer = num + num2;
+            //Console.WriteLine(answer);
+
+            //int num3 = 5;
+            //int num4 = 3;
+            //int answer1 = num3 - num4;
+            //Console.WriteLine(answer1);
+
+            //int num5 = 7;
+            //int num6 = 8;
+            //int answer2 = num5 * num6;
+            //Console.WriteLine(answer2);
+
+            //decimal num7 = 77m;
+            //decimal num8 = 6m;
+            //decimal answer3 = num7 / num8;
+            //Console.WriteLine(answer3);
+
+            //Console.ReadLine();
+
+            //Challenge 7B 2 
+
+            string fname = "Pheobe";
+            string lname = " Morrison";
+            Console.WriteLine(fname + lname);
+            Console.ReadLine(); 
+
+
+        }
+    }
+}
