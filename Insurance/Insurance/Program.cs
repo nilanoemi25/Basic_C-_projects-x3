51a48351697b6a6a0e6848c6c66c7b03daf50c67
nila.noemi25 <nila.noemi25@gmail.com>
2022-10-23 14:48:50 +0100

    insurance
    Module 9

---
+++ Insurance/Insurance/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Insurance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            int age = Console.Read();
+
+            Console.WriteLine("How many DUI violations have you had?");
+
+            int violation = Convert.ToInt32(Console.ReadLine());
+            Console.ReadLine();
+
+            Console.WriteLine("How many speeding tickets have you had?");
+
+            int speeding = Convert.ToInt32(Console.ReadLine());
+            Console.ReadLine();
+
+
+            bool qualification = (age > 15 && violation == 0 && speeding <= 3);
+            Console.WriteLine("Qualification:" + qualification);
+            Console.ReadLine();
+
+        }
+    }
+}
