774ec410272b1a648e3d4c61d0580ef02cb8d226
nila.noemi25 <nila.noemi25@gmail.com>
2022-10-26 14:41:11 +0100

    Price Quote Applicaion

---
+++ Branching_01/Branching_01/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Branching_01
+{
+    class Program
+    {
+        static void Main()
+        {
+            /*Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+
+            if (name == "Amy")
+            { Console.WriteLine("Your name is Amy! ");  }
+
+            if (name == "Noemi" || name == "Amy")
+            { Console.WriteLine("You are awesome."); }
+
+            else if (name == "Denise")
+            { Console.WriteLine("Your name is Denise!"); }
+
+            else
+            { Console.WriteLine("Your name is not recognised!"); } */ 
+
+            int a = 7;
+            int b = 10;
+
+            string c = a > b ? "The universe is ending." : "The universe is doing okay.";
+            Console.WriteLine(c); 
+
+
+        }
+    }
+}
---
+++ Price Quote App/Price Quote App/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Price_Quote_App
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below. What is the weight of your package?");
+            int weight = Convert.ToInt32(Console.ReadLine());
+
+            if (weight >= 50)
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+                Console.ReadLine();
+                    }
+            else if ( weight < 50)
+            {
+                Console.WriteLine("What is the package width? ");
+                int width = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("What is the package height?");
+                int height = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("What is the package lenght?");
+                int lenght = Convert.ToInt32(Console.ReadLine());
+
+                if( width + height + lenght > 50)
+                { Console.WriteLine("Package is too big to be shipped via Package Express. Have a good day.");
+                }
+                else
+                { int price = (width * height * lenght) / 100;
+
+                    Console.WriteLine("The cost of delivery will be £ " + price); 
+                }
+            }
+            
+        }
+    }
+}
