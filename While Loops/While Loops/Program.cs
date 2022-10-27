81dbc0eb2e4f7a6203cc2a931acd73fb0b6d8138
nila.noemi25 <nila.noemi25@gmail.com>
2022-10-27 11:18:39 +0100

    while do while

---
+++ While Loops/While Loops/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace While_Loops
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Guess a number between 1 - 10");
+            int guess = Convert.ToInt32(Console.ReadLine());
+
+            /*
+            while ( guess <= 10)
+            { Console.WriteLine("That is a good guess. Try again");
+                break; 
+                }
+            */
+          
+
+            
+            do { Console.WriteLine("that is a great guess."); }
+
+            while (guess < 10);
+            
+            Console.ReadLine();
+
+
+
+
+
+        }
+    }
+}
