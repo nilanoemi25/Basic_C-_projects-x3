9a75383e484b1d1909ef65be77a4759046c3df5e
nila.noemi25 <nila.noemi25@gmail.com>
2022-10-27 11:59:39 +0100

    String
    Concatenate
    toUpper
    StringBuilder object

---
+++ Strings/Strings/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text; 
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main()
+        {
+            /*string s = "Hello World! + Hello Universe! + Hello Galaxy!";
+            Console.WriteLine(s);
+
+            string uppers = s.ToUpper();
+            Console.WriteLine(uppers); */ 
+
+            StringBuilder x = new StringBuilder("HELLO ");
+
+            x.Append("there.");
+            x.AppendLine("The weather is nice today.");
+            x.AppendLine("It is not raining.");
+            x.Append("Nor is it windy.");
+
+            Console.WriteLine(x);
+            Console.ReadLine(); 
+
+
+
+        }
+    }
+}
