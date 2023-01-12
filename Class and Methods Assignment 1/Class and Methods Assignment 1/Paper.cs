ee6364409bbbdc96bd7048f91c18fe274aad5d8e
nila.noemi25 <nila.noemi25@gmail.com>
2023-01-12 20:41:11 +0000

    Classes and Methods 1

---
+++ Class and Methods Assignment 1/Class and Methods Assignment 1/Paper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Class_and_Methods_Assignment_1
+{
+    class Paper
+    {
+        public static int myMethod(int number)
+        {
+            int result = number + 1;
+            return result; 
+        }
+
+        public static int myMethod(float number)
+        {
+            float result = number + 2;
+            int finalresult = (int)result;
+            return finalresult; 
+        }
+
+        public static int myMethod(string String)
+        {
+            int number = Convert.ToInt32(String);
+            int finalresult = number + 3;
+            return finalresult; 
+        }
+    }
+}
---
+++ Class and Methods Assignment 1/Class and Methods Assignment 1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Class_and_Methods_Assignment_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Paper myobj = new Paper();
+            int thisresult = Paper.myMethod(1);
+
+            Console.WriteLine("the result of using first method is: " + thisresult);
+
+            int thatresult = Paper.myMethod(1.2f);
+            Console.WriteLine("the result of using second method is: " + thatresult);
+
+            int result = Paper.myMethod("3");
+            Console.WriteLine("the result of using third method is: " + result); 
+
+        }
+    }
+}
