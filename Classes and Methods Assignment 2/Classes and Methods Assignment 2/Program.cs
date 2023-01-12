2aa7033bc8ef3e5cf27d2204436b7ddc89bb8f55
nila.noemi25 <nila.noemi25@gmail.com>
2023-01-12 21:08:42 +0000

    Classes and Methods Assignment 2

---
+++ Classes and Methods Assignment 2/Classes and Methods Assignment 2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Classes_and_Methods_Assignment_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rock rock = new Rock();
+
+            Console.WriteLine("Hey user, chose a number:");
+            int userinput = Convert.ToInt32(Console.ReadLine());
+
+         
+
+            try
+            {
+                int optionaluserinput;
+                Console.WriteLine("Thanks, give me another number: (or not)");
+                optionaluserinput = Convert.ToInt32(Console.ReadLine());
+            }
+            
+            catch 
+            {
+                int optionaluserinput = 1;
+                return; 
+       
+            }
+            finally {
+                
+                int granite = Rock.myMethod(userinput, optionaluserinput);
+                Console.WriteLine(granite);
+                Console.ReadLine();
+            }
+
+
+        }
+    }
+}
---
+++ Classes and Methods Assignment 2/Classes and Methods Assignment 2/Rock.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes_and_Methods_Assignment_2
+{
+    class Rock
+    {
+        public static int myMethod(int number, int optionalnumber = 0)
+        {
+            int result = number + optionalnumber + 1;
+            return result; 
+        }
+
+    }
+}
