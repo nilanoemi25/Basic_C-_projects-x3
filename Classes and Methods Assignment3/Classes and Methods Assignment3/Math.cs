c581fc2fb2c6f0240a6c7af960564554d31aaedf
nila.noemi25 <nila.noemi25@gmail.com>
2023-01-13 18:19:03 +0000

    Class and Methods Assignment 3

---
+++ Classes and Methods Assignment3/Classes and Methods Assignment3/Math.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes_and_Methods_Assignment3
+{
+    class Math
+    {
+        public void myMethod(int number, int next)
+        {
+            int result = number + 1;
+
+            Console.WriteLine(next); 
+        }
+
+    }
+}
---
+++ Classes and Methods Assignment3/Classes and Methods Assignment3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Classes_and_Methods_Assignment3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math myMath = new Math();
+
+            myMath.myMethod(1, 8);
+            myMath.myMethod(number: 1, next: 8);
+            Console.ReadLine();
+        }
+    }
+}
