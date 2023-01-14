e44cb573d5880366434203f9895902d545f4d081
nila.noemi25 <nila.noemi25@gmail.com>
2023-01-14 14:46:28 +0000

    class method 4

---
+++ Class Method Assignment 4/Class Method Assignment 4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Class_Method_Assignment_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            School myschool = new School();
+
+            Console.WriteLine("Hi user give me a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+           // myschool.myMethod(number, out int loop, 1); // First Method 
+            //Console.ReadLine();
+
+            //Overload method: 
+            myschool.myMethod(number, 4, out int loop, 1);
+            Console.ReadLine(); 
+        }
+    }
+}
---
+++ Class Method Assignment 4/Class Method Assignment 4/School.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Class_Method_Assignment_4
+{
+    class School
+    {
+        public void myMethod(int number, out int loop, int times = 1)
+
+        {
+            loop = 0;
+
+            for (int i = 0; i < times; i++) 
+            {
+                loop++; 
+                int a = number / 2;
+                Console.WriteLine("The result of the method is: " + a);
+            }
+            
+            
+        }
+
+        public void myMethod(int number, int second, out int loop, int times = 0)
+        {
+
+            loop = 0;
+            
+
+            for (int i = 0; i<times; i++)
+
+            {
+                loop++;
+                int a = number / 2;
+                int b = second / 2;
+                Console.WriteLine("The result of this loop is: " + a + " and " + b); 
+            }
+        }
+
+    }
+}
