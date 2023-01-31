7fc23acc3cba2fcba67afce32ddab72cd4092d5a
nila.noemi25 <nila.noemi25@gmail.com>
2023-01-31 11:02:37 +0000

    Struct Assignment

---
+++ Struct Assignment/Struct Assignment/Number.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Struct_Assignment
+{
+    public struct Number 
+    {
+        public decimal Amount { get; set; }
+    }
+}
---
+++ Struct Assignment/Struct Assignment/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Struct_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number number = new Number();
+            number.Amount = 0.08766m;
+
+            Console.WriteLine(number.Amount);
+            Console.ReadLine(); 
+
+
+        }
+    }
+}
