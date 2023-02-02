265ffe2022702d5a8d3d4a37c8464b6dc167430c
nila.noemi25 <nila.noemi25@gmail.com>
2023-02-02 16:40:15 +0000

    21 Game

--- TwentyOne Game/TwentyOne Game/Card.cs
+++ TwentyOne Game/TwentyOne Game/Card.cs
@@ -12,7 +12,11 @@ namespace TwentyOne_Game
         public Suit Suit { get; set; }
         public Face Face { get; set; }
 
-       
+        public override string ToString()
+        {
+            return String.Format("{0} of {1}", Face, Suit); 
+        }
+
     }
 
     public enum Suit
