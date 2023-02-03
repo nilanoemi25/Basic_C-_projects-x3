48eda3c005859ca887ef38596280e1a8ec0947af
nila.noemi25 <nila.noemi25@gmail.com>
2023-02-03 16:03:19 +0000

    Exception Handling A M8
    Module 8

---
+++ Exception Handling module 8/Exception Handling module 8/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Exception_Handling_module_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try {
+                Console.WriteLine("How old are you?");
+                int age = Convert.ToInt32(Console.ReadLine());
+
+                if ( age <= 0) 
+                {
+                    throw new ArgumentNullException(); 
+                }
+
+                Console.WriteLine("That means you were born in the year of : " + (2023 - age) + ".");
+            }
+            
+            catch (ArgumentNullException)
+            {
+                Console.WriteLine("You have entered a negative or zero digit. Please try again");
+            }
+            catch (Exception )
+            {
+                Console.WriteLine("An error occured, try again. "); 
+            }
+
+        }
+    }
+}
